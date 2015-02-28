86152fc96deff9651902ada6040cbc4e4a41703c
Lena Kotets <kotets@steelkiwi.com>
2015-02-28 23:27:48 +0200

    slider dots

--- new/static/js/script.js
+++ new/static/js/script.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
     $("#slider").owlCarousel({
         loop: true,
         items: 1,
+        //autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        dots: true
     });
 
 });
