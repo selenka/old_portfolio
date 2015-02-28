41496a6aaab48c389c697139b80c00438aa78111
Lena Kotets <kotets@steelkiwi.com>
2015-02-28 23:40:20 +0200

    android icon add

--- new/static/js/script.js
+++ new/static/js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     $("#slider").owlCarousel({
         loop: true,
         items: 1,
-        autoplay:true,
-        autoplayTimeout:3000,
+        //autoplay:true,
+        autoplayTimeout:7000,
         autoplayHoverPause:true,
         dots: true
     });
