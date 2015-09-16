feb86498e95e9074774fbcaac6b657b66c73be89
selenka <lenka@kotets.com>
2015-09-16 15:26:49 +0300

    verta test task

---
+++ verta_media/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $("#slider").lightSlider({
+        item: 1,
+        slideMargin: 0,
+        loop: true,
+        controls: false
+    }); 
+
+
+    //select2 start
+   
+    // function styleSelect(selectClass, additionClass) {
+    //      $(selectClass).select2({
+    //         minimumResultsForSearch: -1,
+    //         containerCssClass: additionClass + "-drop",
+    //         dropdownCssClass: additionClass + "-dropdown",
+    //         dropdownAutoWidth: !0,
+    //         placeholder: function(){
+    //             $(this).data('placeholder');
+    //         },
+    //         width: "style"
+    //     });
+    // }
+    // styleSelect('.select-dir', 'dir');
+    // styleSelect('.select-form', 'form');
+    //select2 end
+
+});
+
+    
